3f063abd0804527477602fa71ef9671139cd3429
lksing <leiaxsing@gmail.com>
2018-07-18 14:08:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "root"
+  end
+  
+  get '/name' do
+    "My name name is Auntie Kate"
+  end
+  
+  get '/hometown' do
+    "My hometown is Indore"
+  end
+  
+  get '/favorite-song' do
+    "My favorite is Indian National Anthem"
+  end
+  
 end
